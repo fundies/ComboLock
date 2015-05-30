d3f8fc3ae04b6739d92e0e9f9afd831ace0f7d61
fundies <cheeseboy16@gmail.com>
2015-05-30 13:54:05 +0000

    done

---
+++ src/combolock/ComboLock.java
@@ -0,0 +1,114 @@
+package combolock;
+
+import java.util.Arrays;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Greg Williamson
+ * @version 0.1
+ */
+public class ComboLock
+{
+    private final int[] combo = new int[3];
+    private final int[] tryCombo = new int[3];
+    
+    private int dial;
+    private int state;
+    
+    /**
+     * Gets the current position of the dial.
+     *
+     * @return the position
+     */
+    public int getDial()
+    {
+        return dial;
+    }
+    
+    /**
+     * Sets the current position of the dial
+     * @param pos set dial to this position
+     */
+    public void setDial(int pos)
+    {
+        dial = pos;
+    }
+    
+    /**
+     * Construct a combination lock with three numbers
+     * @param secret1 first digit of combo
+     * @param secret2 second digit of combo
+     * @param secret3 third digit of combo
+     */
+    public ComboLock(int secret1, int secret2, int secret3) 
+    {
+        combo[0] = secret1;
+        combo[1] = secret2;
+        combo[2] = secret3;
+        
+        state = 0;
+        dial = 0;
+    }
+    
+    /**
+     * Reset the Lock to 0
+     */
+    public void reset() 
+    {
+        state = 0;
+        dial = 0;
+        tryCombo[0] = tryCombo[1] = tryCombo[2] = 0; 
+    }
+    
+    /**
+     * Turn the lock Left
+     * @param ticks number of ticks user moves left
+     */
+    public void turnLeft(int ticks) 
+    {   
+        if (state == 1)
+        {
+            dial += ticks;
+            dial %= 40;
+            tryCombo[state++] = dial;
+        }
+        else
+        {
+            reset();
+        }
+    }
+    
+    /**
+     * Turn the lock Right
+     * @param ticks number of ticks user moves right
+     */
+    public void turnRight(int ticks) 
+    {
+        if ((state == 0) || state == 2)
+        {
+            dial += 40 - (ticks % 40);
+            tryCombo[state++] = dial;
+        }
+        else
+        {
+            reset();
+        }
+    }
+    
+    /**
+     * Test if lock is unlocked
+     * @return returns true if unlocked false if locked
+     */
+    public boolean open() 
+    {
+        Object[] a1 = {combo};
+        Object[] a2 = {tryCombo};
+        return Arrays.deepEquals(a1, a2);
+    }
+}
---
+++ src/combolock/ComboLockTest.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package combolock;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Greg Williamson
+ * @version 0.1
+ */
+public class ComboLockTest
+{
+
+    /**
+     * Test of getDial method, of class ComboLock.
+     */
+    @Test
+    public void testGetDial()
+    {
+        System.out.println("getDial");
+        ComboLock instance = new ComboLock(0, 0, 0);
+        instance.turnRight(1);
+        int result = instance.getDial();
+        assertEquals(39, result);
+    }
+
+    /**
+     * Test of setDial method, of class ComboLock.
+     */
+    @Test
+    public void testSetDial()
+    {
+        System.out.println("setDial");
+        ComboLock instance = new ComboLock(0, 0, 0);
+        instance.setDial(8);
+        int result = instance.getDial();
+        assertEquals(8, result);
+    }
+
+    /**
+     * Test of reset method, of class ComboLock.
+     */
+    @Test
+    public void testReset()
+    {
+        System.out.println("reset");
+        ComboLock instance = new ComboLock(0, 0, 0);
+        instance.setDial(32);
+        instance.reset();
+        int result = instance.getDial();
+        assertEquals(0, result);
+    }
+
+    /**
+     * Test of turnLeft method, of class ComboLock.
+     */
+    @Test
+    public void testTurnLeft()
+    {
+        System.out.println("turnLeft");
+        int ticks = 1;
+        ComboLock instance = new ComboLock(0, 0, 0);
+        
+        instance.turnLeft(ticks);
+        assertEquals(0, instance.getDial());
+        
+        instance.turnRight(0);
+        instance.turnLeft(ticks);
+        
+        assertEquals(1, instance.getDial());
+    }
+
+    /**
+     * Test of turnRight method, of class ComboLock.
+     */
+    @Test
+    public void testTurnRight()
+    {
+        System.out.println("turnRight");
+        int ticks = 1;
+        ComboLock instance = new ComboLock(0, 0, 0);
+        
+        instance.turnRight(ticks);
+        instance.turnRight(ticks);
+        assertEquals(0, instance.getDial());
+        
+        instance.turnRight(ticks);
+        assertEquals(39, instance.getDial());
+    }
+
+    /**
+     * Test of open method, of class ComboLock.
+     */
+    @Test
+    public void testOpen()
+    {
+        System.out.println("open");
+        ComboLock instance = new ComboLock(1, 2, 3);
+        
+        assertEquals(false, instance.open());
+        
+        instance.turnRight(39);
+        assertEquals(false, instance.open());
+        
+        instance.turnLeft(41);
+        assertEquals(false, instance.open());
+        
+        instance.turnRight(39);
+        assertEquals(true, instance.open());
+    }
+
+}
